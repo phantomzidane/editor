84e6ed433825c9339e21076327d74bc120128740
zandroid <30754017+phantomzidane@users.noreply.github.com>
2019-11-11 10:42:14 +0100

    created the toolbar component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,14 +6,16 @@ import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import {MaterialsModule} from './materials/materials.module';
 import { LeftEditorComponent } from './left-editor/left-editor.component';
 import { RightEditorComponent } from './right-editor/right-editor.component';
-import { ContentComponent } from './content/content.component'
+import { ContentComponent } from './content/content.component';
+import { ToolbarComponent } from './toolbar/toolbar.component'
 
 @NgModule({
   declarations: [
     AppComponent,
     LeftEditorComponent,
     RightEditorComponent,
-    ContentComponent
+    ContentComponent,
+    ToolbarComponent
   ],
   imports: [
     BrowserModule,
